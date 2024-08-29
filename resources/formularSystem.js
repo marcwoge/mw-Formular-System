9dca98537da4c23f49d1b56942bb5c6d39941e00
Woge <mpw@ntc-gmbh.com>
2024-08-29 18:06:48 +0200

    debuging messages removed

--- resources/formularSystem.js
+++ resources/formularSystem.js
@@ -2,10 +2,7 @@ window.addEventListener("message", function(event) {
     if (event.data.type === "resizeIframe" && event.data.height) {
         var iframe = document.getElementById('formularIframe');
         if (iframe) {
-            console.log("Original height: " + event.data.height);
-            console.log("Adjusted height: " + (event.data.height + 400));
             iframe.style.height = (event.data.height + 100) + 'px';
-            console.log("Iframe height set to: " + iframe.style.height);
         }
     }
 }, false);
